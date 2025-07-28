07336b0df99ade4012716f622c45e7d80d4bd3c2
thalles.bragatti <thalles.bragatti@VI-2407>
2025-07-28 15:21:43 -0300

    :sparkles: feat: Captada as informações do json e adaptadas para homepage. Criada a estrutura inicial do mostruário de produtos.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 
+const listaProdutos = document.getElementById("mostruario")
+
 
 fetch ("/produtos.json").then(res => res.json()).then(produtos => {
+     
+    produtos.forEach(produto => {
+ 
+       const cardProduto = document.createElement('li')
 
-    console.log(produtos)
+        cardProduto.innerHTML = 
+
+        `<img src=${produto.imagem} alt="Dinosaur" />
+        <h1> ${produto.nome}<h1>
+        <h2> ${produto.preco}<h2>
+        <h2> ${produto.descricao}<h2>
+        `
+
+        listaProdutos.appendChild(cardProduto)
+
+    });
+
+    
 })
+
