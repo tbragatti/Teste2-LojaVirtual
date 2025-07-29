fe618c6df5297b53d3cae6f6b339c1e888e52190
thalles.bragatti <thalles.bragatti@VI-2407>
2025-07-29 16:58:57 -0300

    :sparkles: feat: Implementada a função de criar página de detalhes do produto onclick.

--- js/index.js
+++ js/index.js
@@ -1,27 +1,33 @@
 
 const listaProdutos = document.getElementById("mostruario")
 
-
 fetch ("/produtos.json").then(res => res.json()).then(produtos => {
+
+    
      
     produtos.forEach(produto => {
+
+
  
        const cardProduto = document.createElement('li')
 
         cardProduto.innerHTML = 
 
-        `<div class= "mostruario__container">
+        `<div id= "mostruario__container" onclick="window.location.href='/pages/produto.html?id=${produto.id}'">
         <img src="${produto.imagem}" class="mostruario__imagem" />
         <h1 class= "mostruario__titulo"> ${produto.nome}<h1>
         <h2 class= "mostruario__preco"> R$ ${produto.preco}<h2>
         <h2 class= "mostruario__descricao"> ${produto.descricao}<h2>
         </div>`
         
-
+        
         listaProdutos.appendChild(cardProduto)
+
+        
 
     });
 
     
 })
+
 
