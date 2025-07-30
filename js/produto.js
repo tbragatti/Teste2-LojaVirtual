e4fa39005af462f3b0d3263365b5e35f655c511d
thalles.bragatti <thalles.bragatti@VI-2407>
2025-07-30 14:33:27 -0300

    :sparkles: feat: Finalizado o style. Adaptado para diferentes telas com media queries.

--- js/produto.js
+++ js/produto.js
@@ -8,7 +8,7 @@
                 if (produto) {
                     document.getElementById('detalhes-produto').innerHTML = `
                         <h1 class= "detalhes__nome"> ${produto.nome}</h1>
-                        <img class= "detalhes__imagem"src=" ${produto.imagem}"/>
+                        <img class= "detalhes__imagem" src=" ${produto.imagem}"/>
                          <h2 class="detalhes__preco"> Preço: R$${produto.preco}</h2>
                          <h2 class="descricao__titulo"> Descrição:</h2>
                         <p class="detalhes__descricao"> ${produto.descricao}</p>
