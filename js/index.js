88a8cb93c5599411b7b94dbaf367d7d0010b0fb4
thalles.bragatti <thall@DESKTOP-LF1FBG4>
2025-07-29 23:52:05 -0300

    :sparkles: feat: Adicionado novos estilos. Script dos produtos transferido para um arquivo proprio. Modificado o método de criação de pagina de produto de id para index.

--- js/index.js
+++ js/index.js
@@ -5,20 +5,17 @@ fetch ("/produtos.json").then(res => res.json()).then(produtos => {
 
     
      
-    produtos.forEach(produto => {
-
-
+    produtos.forEach((produto, index) => {
  
        const cardProduto = document.createElement('li')
 
         cardProduto.innerHTML = 
 
-        `<div id= "mostruario__container" onclick="window.location.href='/pages/produto.html?id=${produto.id}'">
+        `<div id= "mostruario__container" onclick="window.location.href='/pages/produto.html?id=${index}'">
         <img src="${produto.imagem}" class="mostruario__imagem" />
         <h1 class= "mostruario__titulo"> ${produto.nome}<h1>
         <h2 class= "mostruario__preco"> R$ ${produto.preco}<h2>
-        <h2 class= "mostruario__descricao"> ${produto.descricao}<h2>
-        </div>`
+         </div>`
         
         
         listaProdutos.appendChild(cardProduto)
---
+++ js/produto.js
@@ -0,0 +1,20 @@
+ const params = new URLSearchParams(window.location.search);
+        const id = params.get("id");
+
+        fetch('/produtos.json')
+            .then(res => res.json())
+            .then(produtos => {
+                const produto = produtos.find((_, index) => index == id);
+                if (produto) {
+                    document.getElementById('detalhes-produto').innerHTML = `
+                        <h1 class= "detalhes__nome"> ${produto.nome}</h1>
+                        <img class= "detalhes__imagem"src=" ${produto.imagem}"/>
+                         <h2 class="detalhes__preco"> Preço: R$${produto.preco}</h2>
+                         <h2 class="descricao__titulo"> Descrição:</h2>
+                        <p class="detalhes__descricao"> ${produto.descricao}</p>
+                        <a class="detalhes__botao" href="/index.html">Voltar para Produtos</a>
+                    `;
+                } else {
+                    document.getElementById('detalhes-produto').innerText = 'Produto não encontrado.';
+                }
+            });
