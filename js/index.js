ef7376932f989c9d2ac0ba21c97ec4284d220e34
Arthur <arthur.oliveira@villelabrasillabs.com>
2025-08-05 14:03:38 -0300

    nota final 8,5/9

--- js/index.js
+++ js/index.js
@@ -1,30 +1,23 @@
-
 const listaProdutos = document.getElementById("mostruario")
 
-fetch ("/produtos.json").then(res => res.json()).then(produtos => {
+fetch("../produtos.json").then(async res => {
+    const produtos = await res.json()
 
-    
-     
     produtos.forEach((produto, index) => {
- 
-       const cardProduto = document.createElement('li')
+        const cardProduto = document.createElement('li')
 
-        cardProduto.innerHTML = 
+        cardProduto.innerHTML =
+            `<a href="../pages/produto?id=${index}">
+                <div id="mostruario__container">
+                    <img src="${produto.imagem}" class="mostruario__imagem" />
+                    <h1 class= "mostruario__titulo"> ${produto.nome}</h1>
+                    <h2 class= "mostruario__preco"> R$ ${produto.preco}</h2>
+                </div>
+            </a>`
 
-        `<div id= "mostruario__container" onclick="window.location.href='/pages/produto.html?id=${index}'">
-        <img src="${produto.imagem}" class="mostruario__imagem" />
-        <h1 class= "mostruario__titulo"> ${produto.nome}<h1>
-        <h2 class= "mostruario__preco"> R$ ${produto.preco}<h2>
-         </div>`
-        
-        
         listaProdutos.appendChild(cardProduto)
-
-        
-
-    });
-
-    
+    })
 })
+
 
 
--- js/produto.js
+++ js/produto.js
@@ -1,12 +1,13 @@
- const params = new URLSearchParams(window.location.search);
-        const id = params.get("id");
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
 
-        fetch('/produtos.json')
-            .then(res => res.json())
-            .then(produtos => {
-                const produto = produtos.find((_, index) => index == id);
-                if (produto) {
-                    document.getElementById('detalhes-produto').innerHTML = `
+fetch("../produtos.json")
+    .then(async res => {
+        const produtos = await res.json();
+        const produto = produtos.find((_, index) => index == id);
+
+        if (produto) {
+            document.getElementById('detalhes-produto').innerHTML = `
                         <h1 class= "detalhes__nome"> ${produto.nome}</h1>
                         <img class= "detalhes__imagem" src=" ${produto.imagem}"/>
                          <h2 class="detalhes__preco"> Preço: R$${produto.preco}</h2>
@@ -14,7 +15,11 @@
                         <p class="detalhes__descricao"> ${produto.descricao}</p>
                         <a class="detalhes__botao" href="/index.html">Voltar para Produtos</a>
                     `;
-                } else {
-                    document.getElementById('detalhes-produto').innerText = 'Produto não encontrado.';
-                }
-            });
+        } else {
+            const a = document.getElementById('detalhes-produto');
+
+            if (a) {
+                a.innerText = 'Produto não encontrado.';
+            }
+        }
+    });
